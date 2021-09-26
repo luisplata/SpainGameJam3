868d25e6119002f67171e3c607e98734caf89700
Luis Plata <www.luisplata@gmail.com>
2021-09-26 12:50:44 -0500

    puzzle finished

--- Assets/MechanichBase/ScriptsFinales/OpenDoorWithComeBack.cs
+++ Assets/MechanichBase/ScriptsFinales/OpenDoorWithComeBack.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 public class OpenDoorWithComeBack : DoSomething
 {
-    [SerializeField] private Animator _animator;
+    [SerializeField] protected Animator _animator;
     public override void Do()
     {
         _animator.SetBool("open", true);
     }
 
-    public void DoAlter()
+    public virtual void DoAlter()
     {
         _animator.SetBool("open", false);
     }
